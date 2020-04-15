41c1617fda065f6da4e5905e5338b3db016cf71f
Khaled Benharrat <benk.devweb@gmail.com>
2020-04-15 22:28:27 +0200

    Changed table

--- public/index.php
+++ public/index.php
@@ -6,12 +6,12 @@ $loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
 $twig = new Twig\Environment($loader);
 
 $products = [
-            'Nike Zoom Pegasus' => 'Very comfortable shoes !!',
-            'Nike React Infinity' => 'Running shoes !!',
-            'Nike Air Zoom Pegasus' => 'Casual shoes !!',
-            'Nike Wildhorse 6' => 'Winter shoes !!',
-            'Nike Vaporfly Flyknit' => 'Summer shoes !!',
-            'Nike CruzrOne' => 'Safety shoes !!'
+            'Nike Zoom Pegasus' => ['Very comfortable shoes !!' => '299'],
+            'Nike React Infinity' => ['Running shoes !!' => '189'],
+            'Nike Air Zoom Pegasus' => ['Casual shoes !!' => '159'],
+            'Nike Wildhorse 6' => ['Winter shoes !!' => '239'],
+            'Nike Vaporfly Flyknit' => ['Summer shoes !!' => '349'],
+            'Nike Cruzr One' => ['Safety shoes !!' => '199']
             ];
 
-echo $twig->render('index.html.twig',['shop'=> $products]);
+echo $twig->render('index.html.twig',['shop' => $products]);
